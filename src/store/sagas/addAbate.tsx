eee1c0c0864e7d79de4160a2a215d71abe9338d0
Bruno Barbosa Franca <bruno.barbosa.franca@gmail.com>
2022-04-17 21:20:15 -0300

    tratativa para nao deixar modificar o abate

--- src/store/sagas/addAbate.tsx
+++ src/store/sagas/addAbate.tsx
@@ -5,10 +5,35 @@ import * as Modal from '~services/modal';
 import {Default} from '~modals';
 import moment from 'moment';
 
+function* procurarAbate(client) {
+  try {
+    const response = database().ref('abate').once('value');
+    const data = [];
+    const res = yield response;
+    res.forEach(r => {
+      data.push(r.toJSON());
+    });
+    const filtro = data.find(a => a.client === client);
+    return filtro ? true : false;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function* addAbate({payload}) {
   try {
     const {date, boi, vaca} = payload;
     let id = moment(date).format('DD-MM-YYYY');
+    const filtro = yield procurarAbate(date);
+    if (filtro) {
+      return Modal.show(() => (
+        <Default
+          cancel={false}
+          title="❌"
+          description="Este fornecedor ja possuir agendamento para esta data!!"
+        />
+      ));
+    }
     database()
       .ref('abate/' + id)
       .set({
